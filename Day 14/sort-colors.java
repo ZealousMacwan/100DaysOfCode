e76efe52cd3c74fbc7c66ab0b888572f595745f2
Zealous Macwan <zealousvmac@gmail.com>
2024-07-02 23:19:42 +0530

    sort-colors

---
+++ Day 14/sort-colors.java
@@ -0,0 +1,27 @@
+// sort-colors
+// https://leetcode.com/problems/sort-colors/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0; i<nums.length; i++){
+           if(nums[i] == 0){
+            count0++;
+           }else if(nums[i] == 1){
+            count1++;
+           }else if(nums[i] == 2){
+            count2++;
+           }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(i<count0)
+                nums[i] = 0;
+            else if(i<count0+count1)
+                nums[i] = 1;
+            else
+                nums[i] = 2;
+        }
+    }
+}
