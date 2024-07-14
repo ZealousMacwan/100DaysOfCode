b0fc8635f74d4133a047bdac5f478b9404593a30
Zealous Macwan <zealousvmac@gmail.com>
2024-07-14 21:23:07 +0530

    search-insert-position

---
+++ Day 26/search-insert-position.java
@@ -0,0 +1,21 @@
+// search-insert-position
+// https://leetcode.com/problems/search-insert-position/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while(left<=right){
+            mid = (left + right)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(target < nums[mid]){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return right+1;
+    }
+}
